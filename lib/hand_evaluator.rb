579019f3f1479f5d5395d53fe773c54785f3a09e
Imi Borbas <imiborbas@gmail.com>
2016-11-29 16:28:52 +0000

    Improve readability.

--- lib/hand_evaluator.rb
+++ lib/hand_evaluator.rb
@@ -2,6 +2,6 @@ require_relative 'hand'
 
 class HandEvaluator
   def return_stronger_hand(left, right)
-    Hand.new(left) > Hand.new(right) ? left : right;
+    Hand.new(left) > Hand.new(right) ? left : right
   end
 end
